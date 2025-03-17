3fd48069298b91faf8b5db0e4d8ddec71c01d306
mm3176 <93787471+mm3176@users.noreply.github.com>
2025-03-17 16:58:11 +0000

    Added sounds each time user clicks a button

--- game.js
+++ game.js
@@ -4,6 +4,11 @@ let buttonColours = ["red", "blue", "green", "yellow"];
 let gamePattern = [];
 let userClickedPattern = [];
 
+function playSound(name){
+    let audio = new Audio("sounds/" + name + '.mp3');
+    audio.play();
+}
+
 function nextSequence(){
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColour = buttonColours[randomNumber];
@@ -11,8 +16,7 @@ function nextSequence(){
 
     $('#' + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    // let audio = new Audio("sounds/" + randomChosenColour + '.mp3');
-    // audio.play();
+    playSound(randomChosenColour);
 }
 
 nextSequence();
@@ -20,4 +24,5 @@ nextSequence();
 $(".btn").on("click", function(){
     let userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
 });
